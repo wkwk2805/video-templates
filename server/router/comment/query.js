fdf649323a7b4de3ec14cac0437bd649e57d8fc3
박의 <wkwk2805@gmail.com>
2020-09-11 14:26:22 +0900

    update query

--- server/router/comment/query.js
+++ server/router/comment/query.js
@@ -2,27 +2,27 @@
 const query = {
   insert: (comment, userUuid, postUuid) =>
     `
-    INSERT INTO vt_comment (
-      comment
-      ,user_uuid
-      ,post_uuid
-    ) VALUES (
-      '${comment}'
-      ,'${userUuid}'
-      ,'${postUuid}'
-    )
-  `,
+      INSERT INTO vt_comment (
+        comment
+        ,user_uuid
+        ,post_uuid
+      ) VALUES (
+        '${comment}'
+        ,'${userUuid}'
+        ,'${postUuid}'
+      )
+    `,
   update: (commentUuid, comment) =>
     `
-  UPDATE vt_comment SET
-    update_date = now()
-    ,comment = '${comment}'
-  WHERE comment_uuid = '${commentUuid}'
-  `,
+    UPDATE vt_comment SET
+      update_date = now()
+      ,comment = '${comment}'
+    WHERE comment_uuid = '${commentUuid}'
+    `,
   delete: (commentUuid) =>
     `
       DELETE FROM vt_comment WHERE comment_uuid = '${commentUuid}'
-  `,
+    `,
 };
 
 module.exports = query;
