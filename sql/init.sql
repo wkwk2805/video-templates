55f3aa4fdf73f75ef1b32c57b9c590b12b56b0de
박의 <wkwk2805@gmail.com>
2020-09-18 09:34:24 +0900

    init.sql 조건에 맞춰서 순서변경

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- Drop table
 
 -- DROP TABLE public.vt_type_list;
@@ -6,6 +8,40 @@ CREATE TABLE public.vt_type_list (
 	type_code varchar NOT NULL,
 	type_name varchar NOT NULL
 );
+
+-- Drop table
+
+-- DROP TABLE public.vt_user;
+
+CREATE TABLE public.vt_user (
+	email varchar NOT NULL,
+	"password" varchar NOT NULL,
+	create_date timestamp NOT NULL DEFAULT now(),
+	update_date timestamp NULL,
+	out_yn varchar(1) NULL DEFAULT 'N'::character varying,
+	phone varchar NULL,
+	user_uuid uuid NOT NULL DEFAULT uuid_generate_v1(),
+	CONSTRAINT vt_user_pk PRIMARY KEY (user_uuid),
+	CONSTRAINT vt_user_un UNIQUE (email)
+);
+CREATE INDEX vt_user_user_uuid_idx ON public.vt_user USING btree (user_uuid);
+
+-- Drop table
+
+-- DROP TABLE public.vt_post;
+
+CREATE TABLE public.vt_post (
+	post_uuid uuid NOT NULL DEFAULT uuid_generate_v1(),
+	title varchar NOT NULL,
+	"content" varchar NOT NULL,
+	read_count int4 NULL,
+	create_date timestamp NULL DEFAULT now(),
+	update_date timestamp NULL,
+	user_uuid uuid NOT NULL,
+	CONSTRAINT vt_post_pk PRIMARY KEY (post_uuid),
+	CONSTRAINT vt_post_fk FOREIGN KEY (user_uuid) REFERENCES vt_user(user_uuid)
+);
+
 -- Drop table
 
 -- DROP TABLE public.vt_comment;
@@ -55,35 +91,6 @@ CREATE TABLE public.vt_like (
 	CONSTRAINT vt_like_fk_2 FOREIGN KEY (comment_uuid) REFERENCES vt_comment(comment_uuid),
 	CONSTRAINT vt_like_fk_3 FOREIGN KEY (user_uuid) REFERENCES vt_user(user_uuid)
 );
--- Drop table
 
--- DROP TABLE public.vt_post;
 
-CREATE TABLE public.vt_post (
-	post_uuid uuid NOT NULL DEFAULT uuid_generate_v1(),
-	title varchar NOT NULL,
-	"content" varchar NOT NULL,
-	read_count int4 NULL,
-	create_date timestamp NULL DEFAULT now(),
-	update_date timestamp NULL,
-	user_uuid uuid NOT NULL,
-	CONSTRAINT vt_post_pk PRIMARY KEY (post_uuid),
-	CONSTRAINT vt_post_fk FOREIGN KEY (user_uuid) REFERENCES vt_user(user_uuid)
-);
--- Drop table
-
--- DROP TABLE public.vt_user;
-
-CREATE TABLE public.vt_user (
-	email varchar NOT NULL,
-	"password" varchar NOT NULL,
-	create_date timestamp NOT NULL DEFAULT now(),
-	update_date timestamp NULL,
-	out_yn varchar(1) NULL DEFAULT 'N'::character varying,
-	phone varchar NULL,
-	user_uuid uuid NOT NULL DEFAULT uuid_generate_v1(),
-	CONSTRAINT vt_user_pk PRIMARY KEY (user_uuid),
-	CONSTRAINT vt_user_un UNIQUE (email)
-);
-CREATE INDEX vt_user_user_uuid_idx ON public.vt_user USING btree (user_uuid);
 
